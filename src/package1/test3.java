6dd3cedb0161c987a9d198522a27ca704daaa6b4
Tahir <taltindasforum@gmail.com>
2022-12-15 22:05:08 +0300

    test3 new changes and group3

--- src/package1/test3.java
+++ src/package1/test3.java
@@ -3,6 +3,8 @@ package package1;
 public class test3 {
 
     public static void main(String[] args) {
-        System.out.println("Wooden Spoon");
+        System.out.println("group3");
     }
+
+
 }
